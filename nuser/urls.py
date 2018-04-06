5725d0d578d9117fbd1afa0642357bb3e124bf4d
dJabber <dJabber@gmail.com>
2018-04-07 00:01:11 +0300

    add logout all view

--- nuser/urls.py
+++ nuser/urls.py
@@ -1,3 +1,4 @@
+from nuser import views
 from django.conf.urls import re_path
 from djoser import views as djoser_views
 from rest_framework_jwt import views as jwt_views
@@ -6,6 +7,7 @@ urlpatterns = [
     re_path(r'^user/view/$', djoser_views.UserView.as_view(), name='user-view'),
     re_path(r'^user/delete/$', djoser_views.UserDeleteView.as_view(), name='user-delete'),
     re_path(r'^user/create/$', djoser_views.UserCreateView.as_view(), name='user-create'),
+    re_path(r'^user/logout/all/$', views.NftgApiUserLogoutAllView.as_view(), name='user-logout-all'),
     # Views are defined in Rest Framework JWT, but we're assigning custom paths.
     re_path(r'^user/login/$', jwt_views.ObtainJSONWebToken.as_view(), name='user-login'),
     re_path(r'^user/login/refresh/$', jwt_views.RefreshJSONWebToken.as_view(), name='user-login-refresh'),
